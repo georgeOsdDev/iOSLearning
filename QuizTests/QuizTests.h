014032112760e063c069c5e46faad12f062e4997
Takeharu Oshida <takeharu.oshida@mobilus.co.jp>
2014-07-29 10:38:34 +0900

    Initial Commit

---
+++ QuizTests/QuizTests.h
@@ -0,0 +1,13 @@
+//
+//  QuizTests.h
+//  QuizTests
+//
+//  Created by Oshida Takeharu on 7/29/14.
+//  Copyright (c) 2014 Oshida Takeharu. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface QuizTests : SenTestCase
+
+@end
---
+++ QuizTests/QuizTests.m
@@ -0,0 +1,32 @@
+//
+//  QuizTests.m
+//  QuizTests
+//
+//  Created by Oshida Takeharu on 7/29/14.
+//  Copyright (c) 2014 Oshida Takeharu. All rights reserved.
+//
+
+#import "QuizTests.h"
+
+@implementation QuizTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in QuizTests");
+}
+
+@end
