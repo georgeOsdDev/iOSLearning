41f3fb989759473de0fc7f419480885459d88725
Takeharu Oshida <takeharu.oshida@mobilus.co.jp>
2014-08-01 15:13:34 +0900

    Chapter 5

--- Hypnosister/Hypnosister/BNRAppDelegate.m
+++ Hypnosister/Hypnosister/BNRAppDelegate.m
@@ -22,13 +22,9 @@
 
 
     //    CGRect firstFrame = CGRectMake(160, 240, 100, 150);
-    CGRect firstFrame = self.window.bounds;
-
-    
-    BNRHypnosisView *firstView = [[BNRHypnosisView alloc] initWithFrame:firstFrame];
-//    firstView.backgroundColor  = [UIColor redColor];
-    
-    [self.window addSubview:firstView];
+//    CGRect firstFrame = self.window.bounds;
+//    BNRHypnosisView *firstView = [[BNRHypnosisView alloc] initWithFrame:firstFrame];
+//    [self.window addSubview:firstView];
 
 //    CGRect secondFrame = CGRectMake(20, 30, 50, 50);
 //    
@@ -37,6 +33,35 @@
 //
 //    [firstView addSubview:secondView];
 
+    
+    //Create CGRects for frames
+    CGRect screenRect = self.window.bounds;
+    CGRect bigRect = screenRect;
+    bigRect.size.width *= 2.0;
+//    bigRect.size.height *= 2.0;
+    
+    //Create a screen-sized scroll view and add it to the window
+    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:screenRect];
+
+    // Force the scroll view to swap its viewing port to one of the views
+    scrollView.pagingEnabled = YES;
+    
+    [self.window addSubview:scrollView];
+    
+    // Create a super-sized hypnosis view and add it to the scroll view
+//    BNRHypnosisView *hypnosisView = [[BNRHypnosisView alloc] initWithFrame:bigRect];
+
+    // Create a screen-sized hypnosis view and add it to the scroll view
+    BNRHypnosisView *hypnosisView = [[BNRHypnosisView alloc] initWithFrame:screenRect];
+    [scrollView addSubview:hypnosisView];
+   
+    // Add a second screen-sized hypnosis view just off screen to the right
+    screenRect.origin.x += screenRect.size.width;
+    BNRHypnosisView *anotherVIew = [[BNRHypnosisView alloc] initWithFrame:screenRect];
+    [scrollView addSubview:anotherVIew]
+    ;
+    // Tell the scroll view how big its content area is
+    scrollView.contentSize = bigRect.size;
     
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
