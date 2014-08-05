b83449d931b94d745984b8db13ba7d45f16e3830
Takeharu Oshida <takeharu.oshida@mobilus.co.jp>
2014-08-05 16:29:21 +0900

    Chapter 7

--- HypnoNerd/HypnoNerd/BNRHypnosisViewController.m
+++ HypnoNerd/HypnoNerd/BNRHypnosisViewController.m
@@ -9,7 +9,7 @@
 #import "BNRHypnosisViewController.h"
 #import "BNRHypnosisView.h"
 
-@interface BNRHypnosisViewController ()
+@interface BNRHypnosisViewController () <UITextFieldDelegate>
 
 @end
 
@@ -20,6 +20,23 @@
     // Create a view
     CGRect frame = [UIScreen mainScreen].bounds;
     BNRHypnosisView *backgroundView = [[BNRHypnosisView alloc] initWithFrame:frame];
+    
+    CGRect textFieldRect = CGRectMake(40, 70, 240, 30);
+    UITextField *textField = [[UITextField alloc] initWithFrame:textFieldRect];
+    
+    // Setting the border style on the text field will allows us to see it more easily
+    textField.borderStyle = UITextBorderStyleRoundedRect;
+    textField.placeholder = @"Hypnotize me";
+    textField.returnKeyType                 = UIReturnKeyDone;
+    textField.autocapitalizationType        = UITextAutocapitalizationTypeWords;
+    textField.autocorrectionType            = UITextAutocorrectionTypeYes;
+    textField.enablesReturnKeyAutomatically = YES;
+    textField.keyboardType                  = UIKeyboardTypeDefault;
+    textField.secureTextEntry               = NO;
+    
+    textField.delegate = self;
+    
+    [backgroundView addSubview:textField];
     
     // Set it as the view of this view controller
     self.view = backgroundView;
@@ -47,6 +64,60 @@
 {
     [super viewDidLoad];
     NSLog(@"BNRHypnosisViewController loaded its view");
+    
 }
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    NSLog(@"%@", textField.text);
+
+    [self drawHypnoticMessage:textField.text];
+    textField.text = @"";
+    [textField resignFirstResponder];
+    
+    return YES;
+}
+
+- (void) drawHypnoticMessage: (NSString *)message
+{
+    for (int i = 0; i < 20; i++){
+        UILabel *messageLabel = [[UILabel alloc] init];
+        messageLabel.backgroundColor = [UIColor clearColor];
+        messageLabel.textColor       = [UIColor whiteColor];
+        messageLabel.text = message;
+        
+        [messageLabel sizeToFit];
+
+        int width = self.view.bounds.size.width - messageLabel.bounds.size.width;
+        int x = arc4random() % width;
+        
+        int height = self.view.bounds.size.height - messageLabel.bounds.size.height;
+        int y = arc4random() % height;
+        
+        
+        CGRect frame = messageLabel.frame;
+        frame.origin = CGPointMake(x, y);
+        messageLabel.frame = frame;
+        
+        [self.view addSubview:messageLabel];
+        
+        UIInterpolatingMotionEffect *motionEffect;
+        motionEffect = [[UIInterpolatingMotionEffect alloc] initWithKeyPath:@"center.x"
+                                                                       type:UIInterpolatingMotionEffectTypeTiltAlongHorizontalAxis];
+        motionEffect.minimumRelativeValue = @-25;
+        motionEffect.maximumRelativeValue = @25;
+        [messageLabel addMotionEffect:motionEffect];
+        
+        motionEffect = [[UIInterpolatingMotionEffect alloc] initWithKeyPath:@"center.y"
+                                                                       type:UIInterpolatingMotionEffectTypeTiltAlongHorizontalAxis];
+        motionEffect.minimumRelativeValue = @-25;
+        motionEffect.maximumRelativeValue = @25;
+        [messageLabel addMotionEffect:motionEffect];
+        
+        
+    }
+    
+}
+
 
 @end
